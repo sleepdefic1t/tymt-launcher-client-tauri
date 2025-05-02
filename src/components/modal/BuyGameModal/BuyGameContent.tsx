37601ebd7ebb4f4d6341f4039748c0c924396d35
RAZGRIZ219104 <razgriz219104@gmail.com>
2025-05-02 21:07:28 +0200

    feat: top up modal

--- src/components/modal/BuyGameModal/BuyGameContent.tsx
+++ src/components/modal/BuyGameModal/BuyGameContent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import numeral from "numeral";
 import { Box, Button, CircularProgress, Stack } from "@mui/material";
 import { CONST_CHAIN_ICONS } from "../../../const/ChainConsts";
@@ -16,6 +16,8 @@ const BuyGameContent = ({ game, purchaseGame }: IPropsBuyGameContent) => {
 
   const [loading, setLoading] = useState<boolean>(false);
 
+  const isInsufficient: boolean = useMemo(() => sxpBalance * sxpPrice > game?.price, [sxpBalance, sxpPrice, game?.price]);
+
   return (
     <Stack direction={"column"} justifyContent={"center"} alignItems={"center"} gap={"24px"}>
       <Box className="fs-40-regular white">Buying a Game</Box>
@@ -32,23 +34,37 @@ const BuyGameContent = ({ game, purchaseGame }: IPropsBuyGameContent) => {
       </Stack>
 
       <Box sx={{ width: "100%", border: "1px solid #ffffff33", borderRadius: "16px" }}>
-        <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} padding={"24px 16px"}>
-          <Stack direction={"column"} gap={"8px"}>
-            <Box className="fs-16-regular light">Your current balance:</Box>
-            <Stack direction={"column"} gap={"4px"}>
-              <Box className="fs-16-regular white">{numeral(sxpBalance).format("0,0.00")} SXP</Box>
-              <Box className="fs-16-regular light">$ {numeral(sxpBalance * sxpPrice).format("0,0.00")}</Box>
+        {isInsufficient ? (
+          <Stack direction={"column"} gap={"16px"} padding={"24px 16px"}>
+            <Box className="fs-20-regular white">There are insufficient funds in your account.</Box>
+
+            <Stack direction={"column"} gap={"8px"}>
+              <Box className="fs-16-regular light">Your current balance:</Box>
+              <Stack direction={"column"} gap={"4px"}>
+                <Box className="fs-16-regular white">{numeral(sxpBalance).format("0,0.00")} SXP</Box>
+                <Box className="fs-16-regular light">$ {numeral(sxpBalance * sxpPrice).format("0,0.00")}</Box>
+              </Stack>
             </Stack>
           </Stack>
-          <Box component={"img"} src={ChevronRightDouble} alt="chevron" sx={{ width: "24px", height: "24px" }} />
-          <Stack direction={"column"} gap={"8px"}>
-            <Box className="fs-16-regular light">Balance after purchase:</Box>
-            <Stack direction={"column"} gap={"4px"}>
-              <Box className="fs-16-regular white">{numeral(sxpBalance - game?.price).format("0,0.00")} SXP</Box>
-              <Box className="fs-16-regular light">$ {numeral((sxpBalance - game?.price) * sxpPrice).format("0,0.00")}</Box>
+        ) : (
+          <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} padding={"24px 16px"}>
+            <Stack direction={"column"} gap={"8px"}>
+              <Box className="fs-16-regular light">Your current balance:</Box>
+              <Stack direction={"column"} gap={"4px"}>
+                <Box className="fs-16-regular white">{numeral(sxpBalance).format("0,0.00")} SXP</Box>
+                <Box className="fs-16-regular light">$ {numeral(sxpBalance * sxpPrice).format("0,0.00")}</Box>
+              </Stack>
+            </Stack>
+            <Box component={"img"} src={ChevronRightDouble} alt="chevron" sx={{ width: "24px", height: "24px" }} />
+            <Stack direction={"column"} gap={"8px"}>
+              <Box className="fs-16-regular light">Balance after purchase:</Box>
+              <Stack direction={"column"} gap={"4px"}>
+                <Box className="fs-16-regular white">{numeral(sxpBalance - game?.price).format("0,0.00")} SXP</Box>
+                <Box className="fs-16-regular light">$ {numeral((sxpBalance - game?.price) * sxpPrice).format("0,0.00")}</Box>
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
+        )}
       </Box>
 
       <Button className={"red-button fw"} onClick={purchaseGame}>
@@ -58,6 +74,8 @@ const BuyGameContent = ({ game, purchaseGame }: IPropsBuyGameContent) => {
               color: "#F5EBFF",
             }}
           />
+        ) : isInsufficient ? (
+          "Top up balance"
         ) : (
           "Purchase"
         )}
